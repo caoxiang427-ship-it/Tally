9e64c889993fb82f471213f094f7bf67d73a5077
Cao Xiang <caoxiang427@gmail.com>
2026-08-01 17:12:15 +0800

    Add Vercel domain to CORS

--- backend/api.py
+++ backend/api.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://tally-indol.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
